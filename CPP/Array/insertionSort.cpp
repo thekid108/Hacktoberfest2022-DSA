8093513a2717edfe1081a9f7115490a355f74e43
amee-w-ali <ameerwajidali@gmail.com>
2022-10-08 23:47:05 +0530

    error: show automatically in vs chnage tab

--- CPP/Array/insertionSort.cpp
+++ CPP/Array/insertionSort.cpp
@@ -1,38 +1,37 @@
+//Insertion Sort Algorithm
+
+
 #include <bits/stdc++.h>
 using namespace std;
-
-void insertionSort(int arr[], int n)
+void insertionSort(int a[], int n)
 {
-    int i, key, j;
-    for (i = 1; i < n; i++)
-    {
-        key = arr[i];
-        j = i - 1;
-
-        while (j >= 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
+	int i, o, j;
+	for (i = 1; i < n; i++)
+	{
+		o = a[i];
+		j = i - 1;
+		while (j >= 0 && a[j] > o)
+		{
+			a[j + 1] = a[j];
+			j = j - 1;
+		}
+		a[j + 1] = o;
+	}
 }
-
-void printArray(int arr[], int n)
+void printArray(int a[], int n)
 {
-    int i;
-    for (i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+	int i;
+	for (i = 0; i < n; i++)
+		cout << a[i] << " ";
+	cout << endl;
 }
 
 int main()
 {
-    int arr[] = {12, 4, 6, 2, 44, 32};
-    int N = sizeof(arr) / sizeof(arr[0]);
-
-    insertionSort(arr, N);
-    printArray(arr, N);
-
-    return 0;
+	int arr[] = { 5,23,10,2,56,4 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	insertionSort(arr, n);
+	printArray(arr, n);
+	return 0;
 }
+// This is code is contributed by arghadip biswas
