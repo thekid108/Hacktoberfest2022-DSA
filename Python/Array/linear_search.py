e7f4695f932c59e203c6b15a556f88c577ea9e83
shubharthaksangharsha <20bcs6872@cuchd.in>
2022-10-09 11:12:25 +0530

    Modified Linear Search in more Pythonic and Documented Style

--- Python/Array/linear_search.py
+++ Python/Array/linear_search.py
@@ -1,19 +1,43 @@
-def linearsearch(nums, N, target):
-    for i in range(N):
-        if (nums[i] == target):
+'''
+File created by @shubharthaksangharsha
+Task: Updated Linear Search in more Pythonic Way 
+'''
+from typing import List
+
+def linear_search(arr: List[int], element: int) -> int:
+    '''
+    Returns the index of `element` from arr of list if found else return -1
+
+    Params:
+        arr(List): list of int elements
+        element(int): element to be searched
+
+    Returns:
+        int: index of element if found else return -1 
+    '''
+    for i, v in enumerate(arr):
+        if v == element:
             return i
     return -1
- 
- 
-# Driver Code
 
-nums = list(map(int,input().split()))
-target = int(input())
-N = len(nums)
- 
-# Function call
-ans = linearsearch(nums, N, target)
-if(ans == -1):
-    print("Element is not present in array")
-else:
-    print("Element is present at index", ans)
+def check_result(ans: int, element: int) -> None:
+    '''
+    Print the answer of linear_search
+
+    Params:
+        ans(int): answer of linear_search funct
+        element(int): element to be searched
+
+    Returns:
+       Returns None 
+    '''
+    if ans != -1:
+        print(f"Element {element} found at index {ans}")
+    else:
+        print(f"Element {element} not found at any index")
+    
+
+if __name__ == "__main__":
+    arr = list(map(int, input("Please Enter the List of elements sepereated by space: ").split()))
+    element = int(input("Please Enter the element to be searched: "))
+    check_result(linear_search(arr, element), element)
